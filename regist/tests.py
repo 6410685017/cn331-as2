b31117f469e616bd8feef2c57356de63947e4475
Pitchakan Leephud <6410615113@cn103>
2023-10-22 11:16:17 +0700

    test

--- regist/tests.py
+++ regist/tests.py
@@ -1,3 +1,91 @@
 from django.test import TestCase
+from django.test import TestCase, Client, SimpleTestCase
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from .models import Student, Subject, SubjectStudentList
+from .views import sublist, enroll
+from users.views import remove
 
-# Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_sublist_urls_is_resolved(self):
+        url = reverse('regist:sublist')
+        print(resolve(url))
+        self.assertEqual(resolve(url).func,sublist)
+
+    def test_enroll_urls_is_resolved(self):
+        url = reverse('regist:enroll',args=[1])
+        print(resolve(url))
+        self.assertEqual(resolve(url).func,enroll)
+
+class TestViews(TestCase):
+    
+    def setUp(self):
+        user1 = User.objects.create_user(username='0000', password='passcn331')
+        student = User.objects.get(student='0000', id=0, is_staff=False)
+        # User.objects.get()
+        self.student1 = Student.objects.create(user=student.username, s_id=0)
+        self.subject = Subject.objects.create(
+            sub_id='CN202',
+            name='Data Structure and Algorithm I',
+            semester=1,
+            year=2023,
+            capacity=3,
+            status=True
+        )
+        
+        user1.save()
+        
+
+        self.sublist_url = reverse('regist:sublist')
+        self.enroll_url = reverse('regist:enroll', args=[self.subject])
+        self.remove_url = reverse('users:remove', args=[self.subject])
+        self.client.login(username=user1.username, password=user1.password)
+
+    def test_sublist(self):
+        c = Client()
+        response = c.get(self.sublist_url)
+        self.assertCountEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'regist/sublist.html')
+
+#     def test_enroll(self):
+#         c = Client()
+#         response = c.get(self.enroll_url)
+
+#         self.assertCountEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'regist/sublist.html') 
+    
+#     def test_remove(self):
+#         c = Client()
+#         response = c.get(self.remove_url)
+
+#         self.assertCountEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'users/index.html') 
+    
+
+ 
+# class TestModel(TestCase):
+#     def setUp(self):
+#         user1 = User.objects.create_user(username='0000', password='passcn331')
+#         self.student1 = Student.objects.create(user=user1.username, s_id=0)
+#         self.subject = Subject.objects.create(
+#             sub_id='CN101',
+#             name='INTRODUCTION TO COMPUTER PROGRAMMING',
+#             semester=1,
+#             year=2023,
+#             capacity=3,
+#             status=True
+#         )
+#         self.subjectstudentList = SubjectStudentList.objects.create(students=self.student1, subject=self.subject)
+#         user1.save()
+
+#     def test_student_str(self):
+#         self.assertEquals(self.student1.__str__(), self.student1.user)
+
+#     def test_subject_str(self):
+#         self.assertEquals(self.subject.__str__(), self.subject.sub_id + ' ' + self.subject.sub_name)
+
+#     def test_subjectstudent_str(self):
+#         self.assertEquals(self.subjectstudentList.__str__(), self.student1.user + ' ' 
+#                           + self.subject.sub_id + ' ' + self.subject.sub_name)
+    
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,84 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import index, login_view, logout_view, remove
+from regist.models import Student, Subject
+# from regist.views import enroll
 
 # Create your tests here.
+class TestUrl(SimpleTestCase):
+    def test_index(self):
+        url = reverse('users:index')
+        self.assertEqual(resolve(url).func, index)
+
+    def test_login(self):
+        url = reverse('users:login')
+        self.assertEqual(resolve(url).func, login_view)
+
+    def test_logout(self):
+        url = reverse('users:logout')
+        self.assertEqual(resolve(url).func, logout_view)
+    
+    # def test_remove(self):
+    #     url = reverse('users:index')
+    #     self.assertEqual(resolve(url).func, remove, login_view) 
+    
+
+class TestViews(TestCase):
+    def test_not_login(self):
+        c = Client()
+        response = c.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_success(self):
+        c = Client()
+        url = reverse('users:login')
+        response = c.post(url, {
+            'username': '0000',
+            'password': 'passcn331'
+        })
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_unsuccess(self):
+        url = reverse('users:login')
+        response = self.client.post(url, {
+            'username': '0000',
+            'password': 'wrongpass'
+        })
+
+        self.assertEqual(response.status_code, 200)
+   
+    def test_logout_is_success(self):
+        url = reverse("users:logout")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+    
+    # def test_removesubject(self):
+    #     response = self.client.post(self.removesubject_url)
+
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'regist/sublist.html')
+
+    # def test_removesubject_did_not_regist(self):
+    #     subject = Subject.objects.create(
+    #         sub_id='CN331',
+    #         sub_name='Software Engineering',
+    #         semester=1,
+    #         year=2023,
+    #         capacity=2,
+    #         status=True
+    #     )
+    #     url = reverse('users:index', args=[subject.sub_id])
+    #     response = self.client.post(url)
+
+    #     self.assertEqual(response.status_code, 400)
+    #     self.assertTemplateUsed(response, 'users/index.html')
+
+    # def test_removesubject_get(self):
+    #     response = self.client.get(self.remove_url)
+
+    #     self.assertEqual(response.status_code, 400)
+    #     self.assertTemplateUsed(response, 'regist/sublist.html')
